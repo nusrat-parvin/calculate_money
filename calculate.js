5232a9eb442812b5ea2cf4a5cd1b2fb29ce454fc
Nusrat Parvin <nusrat.shumona@gmail.com>
2023-06-11 21:40:10 +0600

    all calculation completed

--- calculate.js
+++ calculate.js
@@ -11,7 +11,7 @@ function setElementValue(elementId , value){
     newElementValue.innerText = value;
 }
 
-//add event listener to calculate button
+//add event handler to calculate button
 document.getElementById('btn-calc').addEventListener('click' , function(){
 
     //get all the field and value using id
@@ -28,6 +28,25 @@ document.getElementById('btn-calc').addEventListener('click' , function(){
     //set balance after deducting from income
     const newBalanceValue = incomeField - totalExpenseAmount;
     setElementValue('net-balance' , newBalanceValue);
+ 
+});
+// add event handler to save button
+document.getElementById('btn-save').addEventListener('click' , function(){
+    const getIncomeFieldToSave = getInputFieldValueById('income-field');
+    const getSaveValueField = getInputFieldValueById('savings-field');
 
-    
+    const simplifiedValueOfSaveField = getSaveValueField / 100;
+    const parcentAmount = getIncomeFieldToSave * simplifiedValueOfSaveField;
+
+    //set the new amount to saving amount
+    setElementValue('saved-amount' , parcentAmount);
+
+    //get balance field and deduct amount and set the amount to remaining balance
+    const getBalanceAmount = document.getElementById('net-balance');
+    const currentAmountOfBalance = getBalanceAmount.innerText;
+
+    //deduct the value and set
+    const remainingBalance = currentAmountOfBalance - parcentAmount;
+    setElementValue('final-amount' , remainingBalance);
+
 })
