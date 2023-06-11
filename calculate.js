1d9ba6cfa4f40a43aafb9de28cf5c867308a5f76
Nusrat Parvin <nusrat.shumona@gmail.com>
2023-06-11 23:33:39 +0600

    complete

--- calculate.js
+++ calculate.js
@@ -1,4 +1,9 @@
 function getInputFieldValueById(elementId){
+// validate number
+// if (typeof elementId === "string" || typeof elementId === " " || typeof elementId < 0) {
+//             alert('Please enter a valid number');
+//         }
+
     const getInputField = document.getElementById(elementId);
     const inputFieldValue = getInputField.value;
     const convertValue = parseFloat(inputFieldValue);
@@ -11,6 +16,19 @@ function setElementValue(elementId , value){
     newElementValue.innerText = value;
 }
 
+// validation || typeof value === " " || typeof value < 0)
+// function isNumber(value) {
+//     if (typeof value === "string"){
+//         alert('Please enter a valid number');
+//     }
+//     else if(typeof value === " "){
+//         alert("all field are required to be fill");
+//     }
+//     else if(typeof value < 0){
+//         alert("numbers can not be negative");
+//     }
+// }
+
 //add event handler to calculate button
 document.getElementById('btn-calc').addEventListener('click' , function(){
 
@@ -20,10 +38,16 @@ document.getElementById('btn-calc').addEventListener('click' , function(){
     const rentExpenseField = getInputFieldValueById('rent-expense');
     const clothesExpenseField = getInputFieldValueById('clothes-expense');
     
-    //calculate total expense
+    //calculate total expense and set the total expense
     const totalExpenseAmount = foodExpenseField + rentExpenseField + clothesExpenseField;
-    //set the total expense
-    setElementValue('total-expense' , totalExpenseAmount);
+
+    //validate if expense is more the income
+    if(totalExpenseAmount > incomeField){
+        alert("Your expense is more then your income");
+    }
+    else{
+        setElementValue('total-expense' , totalExpenseAmount);
+    }
 
     //set balance after deducting from income
     const newBalanceValue = incomeField - totalExpenseAmount;
@@ -34,16 +58,30 @@ document.getElementById('btn-calc').addEventListener('click' , function(){
 document.getElementById('btn-save').addEventListener('click' , function(){
     const getIncomeFieldToSave = getInputFieldValueById('income-field');
     const getSaveValueField = getInputFieldValueById('savings-field');
+    const getBalanceAmount = document.getElementById('net-balance');
+
+    //validate saving amount
+    if(getSaveValueField > 100){
+        alert("saving is more then income");
+    }
+    else{
+        alert("There is no income amount");
+    }
 
     const simplifiedValueOfSaveField = getSaveValueField / 100;
     const parcentAmount = getIncomeFieldToSave * simplifiedValueOfSaveField;
 
-    //set the new amount to saving amount
-    setElementValue('saved-amount' , parcentAmount);
-
     //get balance field and deduct amount and set the amount to remaining balance
-    const getBalanceAmount = document.getElementById('net-balance');
     const currentAmountOfBalance = getBalanceAmount.innerText;
+
+    //validate savings is more then balance
+    if(parcentAmount > currentAmountOfBalance){
+        alert("you can't save more then your remaining income");
+    }
+    else{
+        //set the new amount to saving amount
+        setElementValue('saved-amount' , parcentAmount);
+    }
 
     //deduct the value and set
     const remainingBalance = currentAmountOfBalance - parcentAmount;
