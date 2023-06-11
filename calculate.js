3771470d5b4623f51c607a635026d6220d6f813e
Nusrat Parvin <nusrat.shumona@gmail.com>
2023-06-11 20:46:39 +0600

    first step calculation done

--- calculate.js
+++ calculate.js
@@ -1,8 +1,33 @@
 function getInputFieldValueById(elementId){
     const getInputField = document.getElementById(elementId);
-    const inputFieldValue = getInputField.ariaValueMax;
+    const inputFieldValue = getInputField.value;
     const convertValue = parseFloat(inputFieldValue);
     return convertValue;
 }
 
+//set the value at it's place
+function setElementValue(elementId , value){
+    const newElementValue = document.getElementById(elementId);
+    newElementValue.innerText = value;
+}
+
 //add event listener to calculate button
+document.getElementById('btn-calc').addEventListener('click' , function(){
+
+    //get all the field and value using id
+    const incomeField = getInputFieldValueById('income-field');
+    const foodExpenseField = getInputFieldValueById('food-expense');
+    const rentExpenseField = getInputFieldValueById('rent-expense');
+    const clothesExpenseField = getInputFieldValueById('clothes-expense');
+    
+    //calculate total expense
+    const totalExpenseAmount = foodExpenseField + rentExpenseField + clothesExpenseField;
+    //set the total expense
+    setElementValue('total-expense' , totalExpenseAmount);
+
+    //set balance after deducting from income
+    const newBalanceValue = incomeField - totalExpenseAmount;
+    setElementValue('net-balance' , newBalanceValue);
+
+    
+})
